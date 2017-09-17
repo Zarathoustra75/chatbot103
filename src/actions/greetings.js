1ae5e58a07db151a19508ae496c4200b687b26e5
Zarathoustra75 <david.fradel@gmail.com>
2017-09-17 21:55:20 +0200

    bugs fixs

--- src/actions/greetings.js
+++ src/actions/greetings.js
@@ -4,22 +4,12 @@ const formatter = require('../formatter')
 export default async function yes(res, payload) {
     console.log('GREETINGS')
 
-    let replies = []
-    let data = await agent('GET', 'https://graph.facebook.com/v2.6/' + res.conversationToken + '/')
-        .query({fields: 'first_name', access_token: process.env.FBGRAPH_TOKEN})
-
-    data = JSON.parse(data.text)
-
-    let first_name = data["first_name"]
-
+    const replies = []
     const greetings = [
-        `Bonjour ${first_name} !`,
-        `Salut ${first_name} !`,
-        `Hey ${first_name}  ;)`,
-        `Yo ${first_name}  ;)`,
+        'Bonjour',
+        'Salut !',
+        'Hey ;)',
     ]
-
     replies.push(formatter.formatMsg(greetings[Math.floor(Math.random() * greetings.length)]))
     return replies
-
 }
